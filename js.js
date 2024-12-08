92dfc24176155fc4551a25f7280c170768398ae1
xop141 <lowsens400@gmail.com>
2024-12-08 07:30:09 -0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,236 @@
+//.1//var num = 11116;
+//let b = 6, c = 0
+//var digits = num.toString().split('');
+//var realDigits = digits.map(Number)
+//for (let i = 0; i < realDigits.length; i++) {
+//   if (realDigits[i-1]==b) {      
+//       c = c + 1
+//   }
+//} console.log(c);
+
+//2//let a = 15
+//let b = 35
+//let x =[]
+//for (let i = 1; i <= a; i++) {
+//   if (b%i ==0 && a%i ==0) {
+//      x.push (i)
+//   }
+//}
+//console.log(Math.max(...x));
+
+//.3//let a = [100,101,30,101,25] 
+//for (let i = 0; i < 5; i++) {
+//   console.log(a.indexOf(Math.max(...a))+1);
+//    a[a.indexOf(Math.max(...a))] = 0
+//}
+
+//.4/const num = 105501;
+//const digits = Array.from(num.toString(), Number);
+//const x = digits.length
+//let p = 0
+//for (let i = 0; i < digits.length/2; i++){
+//    //console.log(digits[x-1-i]);
+//    p = p + digits[x-1-i]*Math.pow(10, i)
+//} 
+//let n = Math.floor(num/Math.pow(10, digits.length/2))
+//let rev = 0;
+//while (n > 0) {
+// rev = rev * 10 + n % 10;
+// n = Math.floor(n / 10);
+//}
+//if (rev==p) {
+//    console.log("yes");
+//}else{
+//    console.log("no");   
+//}
+
+//.5//let ar = [2, 2, 3, 4, 5, 6, 7, 8,]
+//let a = ar.length
+//let n = 0 //tegsh too
+//for (let i = 0; i < ar.length; i++) {
+//    if (ar[i]%2==0) {
+//        n = n + 1
+//    }   
+//} if (a-n>n) {
+//    console.log("yes");
+//} else{
+//    console.log("no");
+//}
+
+//7//let a = 3
+//let b = 5
+//let x = 0
+//for (let i = 0; i <= b; i++) {
+//    x = x + Math.pow(a, b-i)
+//} console.log(x);
+
+
+
+
+  
+   
+   
+
+   
+   
+
+
+
+   
+
+   
+   
+
+      
+      
+       
+   
+   
+   
+ 
+   
+  
+   
+   
+   
+   
+   
+  
+   
+   
+  
+   
+   
+   
+   
+   
+
+
+
+
+
+
+
+
+    
+    
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+   
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
